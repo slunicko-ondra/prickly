bd6d14d4eed4b863b8da134501b62c83ae9c153e
Ondřej Ježek <xjezek2@fi.muni.cz>
2024-05-12 15:10:33 +0200

    add ui to main window

---
+++ PricklyApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace PricklyApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        string [] projects = new []{"project1", "project2", "project3"};
+        ProjectComboBox.ItemsSource = projects;
+    }
+
+
+    private void StartStopButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (StartStopButton.Content.ToString() == "Stop")
+        {
+            StartStopButton.Content = "Start";
+            return;
+        }
+        StartStopButton.Content = "Stop";
+    }
+}
